03ae58fe04b4c472a3edb10bf054ebd77a071ce5
Mike Morrison <tellmike@outlook.com>
2018-05-31 23:52:58 -0600

    Inital Commit to Blog Code branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { NgxMdModule } from 'ngx-md';
+import { FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    NgxMdModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/home.md
@@ -0,0 +1,14 @@
+# Random Thoughts #
+
+This blog is mainly for dumping my thoughts on different technologies and documenting things that I believe need to be documented.
+
+---
+
+Some of the content you can expect to see:
+
+* Angular
+* React
+* C#
+* F#
+* JavaScript / TypeScript
+* SharePoint Online
